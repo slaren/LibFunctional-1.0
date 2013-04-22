f3b6b920bff932581a1465e11a71694b836b6af8
slaren <ddevesa@gmail.com>
2013-04-22 18:42:45 +0200

    added a bunch more of functions, updated some of the documentation, changed some functions to return both a index and a value

--- functional.lua
+++ functional.lua
@@ -4,23 +4,15 @@ local lib = LibStub and LibStub:NewLibrary("LibFunctional-1.0", version) or {}
 if not lib then return end
 
 -- globals
-local tinsert = table.insert
-local tsort = table.sort
-local pairs, select = pairs, select
-local math_min, math_max, math_floor = math.min, math.max, math.floor
+local tinsert, tsort = table.insert, table.sort
+local pairs, select, type, unpack = pairs, select, type, unpack
+local math_min, math_max, math_floor, math_random = math.min, math.max, math.floor, math.random
 
 
 --[[
 TODO:
-	slice
-	foldr
-	shuffle
-	flatten
-	zip
-	unzip
-	index_of
-	last_index_of
-	range
+	range([start], stop[, step])
+	bind(fn, arg1[, arg2...])
 ]]
 
 -- table functions
@@ -55,7 +47,126 @@ lib.pairs = function(t)
 	return r
 end
 
+--- Returns true if //t1// has the same keys and values than //t2//.
+-- @paramsig t1, t2[, shallow]
+-- @param t1 first table.
+-- @param t2 second table.
+-- @param shallow if false or omitted, tables values inside the tables are compared recursively, otherwise they are compared by their reference.
+lib.equal = function(t1, t2, shallow)
+	if #t1 ~= #t2 then return false end
+	for k, v in pairs(t1) do
+		if type(v) == "table" and not shallow then
+			if not lib.equal(v, t2[k]) then return false end
+		elseif t2[k] ~= v then return false end
+	end
+	return true
+end
+
+-- Returns a copy of the table //t// with is values as keys and its keys as values
+-- @param t the input table.
+lib.invert = function(t)
+	local r = {}
+	for k, v in pairs(t) do
+		r[v] = k
+	end
+	return r
+end
+
 -- list functions
+
+--- Returns a copy of a portion of the list //l//.
+-- @paramsig l, b[, e]
+-- @param l the input list.
+-- @param b the first index to copy. If negative, indicates an offset from the end of the list.
+-- @param e optional, the last index to copy. If omitted, the list is copied through the end. If negative, indicates an offset from the end of the list.
+lib.slice = function(l, b, e)
+	local r = {}
+	local len = #l
+	b = b or 1
+	e = e or len
+	b = b < 0 and (len + b + 1) or b
+	e = e < 0 and (len + e) or e
+	for i = b, e do
+		r[i - b + 1] = l[i]
+	end
+	return r
+end
+
+--- Shuffles the list //l// in-place using the Fisher–Yates algorithm and returns it.
+-- @param l the input list.
+lib.shuffle = function(l)
+	local j = #l
+	while (j > 0) do
+		local i = math_random(j)
+		local tmp = l[i]
+		l[i] = l[j]
+		l[j] = tmp
+		j = j - 1
+	end
+	return l
+end
+
+--- Returns a copy of the list //l// shuffled using the Fisher–Yates algorithm.
+-- @param l the input list.
+lib.shuffled = function(l)
+	return lib.shuffle(lib.clone(l))
+end
+
+--- Returns a copy of the list //l// with any nested lists flattened to a single level.
+-- @paramsig l[, shallow]
+-- @param l the input list.
+-- @param shallow optional, if set to true only flattens a single level
+lib.flatten = function(l, shallow, output)
+	local r = output or {}
+	local len = #l
+	for i = 1, len do
+		local v = l[i]
+		if type(v) == "table" then
+			local lenj = #v
+			for j = 1, lenj do
+				local vj = v[j]
+				if type(vj) == "table" and not shallow then
+					lib.flatten(vj, shallow, r)
+				else
+					tinsert(r, vj)
+				end
+			end
+		else
+			tinsert(r, v)
+		end
+	end
+	return r
+end
+
+
+--- Takes any number of lists and returns a new list where each element is a list of the values in all the passed lists at that position.
+-- If one list is shorter than the others, excess elements of the longer lists are discarded
+-- @param ... any number of input lists.
+lib.zip = function(...)
+	local ls = { ... }
+	local n = #ls
+	if n == 0 then return {} end
+	local len = lib.reduce(ls, function(r, v) return math_min(r, #v) end, #ls[1])
+	local r = {}
+	for i = 1, len do
+		local v = {}
+		for j = 1, n do
+			v[j] = ls[j][i]
+		end
+		r[i] = v
+	end
+	return r
+end
+
+--- Undoes a zip operation.
+-- @param l a list of lists.
+lib.unzip = function(l)
+	return unpack(lib.zip(unpack(l)))
+end
+
+-- index_of(l, v)
+-- last_index_of(l, v)
+-- range([start], stop[, step])
 
 --- Returns a shallow copy of the list //l//.
 -- @param l the input list.
@@ -118,16 +229,46 @@ lib.reverse = function(l)
 	return r
 end
 
---- Returns the first value in list //l// that passes the truth test //fn//.
+--- Returns the first value and its index in list //l// that is equal to any of the values passed.
+-- @param l the input list.
+-- @param ... one or more values to search for.
+lib.find_first_of = function(l, ...)
+	local vs = { ... }
+	local r = {}
+	local len = #l
+	for i = 1, len do
+		local lv = l[i]
+		if lib.contains(vs, lv) then
+			return lv, i
+		end
+	end
+end
+
+--- Returns the last value and its index in list //l// that is equal to any of the values passed.
+-- @param l the input list.
+-- @param ... one or more values to search for.
+lib.find_last_of = function(l, ...)
+	local vs = { ... }
+	local r = {}
+	local len = #l
+	for i = len, 1, -1 do
+		local lv = l[i]
+		if lib.contains(vs, lv) then
+			return lv, i
+		end
+	end
+end
+
+--- Returns the first value and its index in list //l// that passes the truth test //fn//.
 -- @param l the input list.
 -- @param fn the truth test function.
-lib.find = function(l, fn)
+lib.find_if = function(l, fn)
 	local r = {}
 	local len = #l
 	for i = 1, len do
 		local v = l[i]
 		if fn(v) then
-			return v
+			return v, i
 		end
 	end
 end
@@ -166,12 +307,14 @@ lib.sorted_index = function(l, v)
 	return lo
 end
 
---- Performs a binary search on sorted list //l// for value //v// and returns it if found.
+--- Performs a binary search on sorted list //l// for value //v// and returns its index if found
 -- @param l the input sorted list.
 -- @param v the value to search for.
 lib.binary_search = function(l, v)
 	local i = lib.sorted_index(l, v)
-	return l[i] == v and v or nil
+	if l[i] == v then
+		return i
+	end
 end
 
 --- Inserts a value //v// in a sorted list //l// and returns it.
@@ -185,6 +328,7 @@ end
 
 --- Returns a reduction of the list //l// based on the left associative application of the function //fn//.
 -- **aliases**: //foldl//
+-- @paramsig l, fn[, initial]
 -- @param l the input list.
 -- @param fn a function receiving two values representing the result of the previous application of this function and the next value in the list //l//.
 -- @param initial an optional initial value to be passed together with the first value of the list //l// to the function //fn//. If omitted, the first call is passed the two first values in the list //l// instead.
@@ -198,6 +342,22 @@ lib.reduce = function(l, fn, initial)
 	return r
 end
 lib.foldl = lib.reduce
+
+--- Returns a reduction of the list //l// based on the right associative application of the function //fn//.
+-- **aliases**: //foldr//
+-- @paramsig l, fn[, initial]
+-- @param l the input list.
+-- @param fn a function receiving two values representing the result of the previous application of this function and the previous value in the list //l//.
+-- @param initial an optional initial value to be passed together with the last value of the list //l// to the function //fn//. If omitted, the first call is passed the two last values in the list //l// instead.
+lib.reduce_right = function(l, fn, initial)
+	local s = initial and #l or #l - 1
+	local r = initial and initial or l[#l]
+	for i = s, 1, -1 do
+		r = fn(r, l[i])
+	end
+	return r
+end
+lib.foldr = lib.reduce_right
 
 --- Returns a sum of all the values in the list //l//.
 -- @param l the input list.
@@ -218,6 +378,7 @@ lib.max = function(l)
 end
 
 --- Performs an in-place sort of the list //l//.
+-- @paramsig l[, comp]
 -- @param l the input list
 -- @param comp an optional comparison function that receives two values and returns true when the first is less than the second.
 lib.sort = function(l, comp)
@@ -226,6 +387,7 @@ lib.sort = function(l, comp)
 end
 
 --- Returns a sorted copy of the list //l//.
+-- @paramsig l[, comp]
 -- @param l the input list.
 -- @param comp an optional comparison function that receives two values and returns true when the first is less than the second.
 lib.sorted = function(l, comp)
@@ -288,6 +450,7 @@ lib.union = function(...)
 end
 
 --- Returns a copy of the list //l// with any duplicate values removed.
+-- @paramsig l[, is_sorted[, fn]]
 -- @param l the input list.
 -- @param is_sorted an optional argument specifying if the list is sorted, allowing to use a more efficient algorithm.
 -- @param fn an optional function that is applied to each value in the list before performing the comparison.
--- tests.lua
+++ tests.lua
@@ -1,22 +1,20 @@
-lib = require("functional");
+local fn = require("functional");
 
 (function()
 	local function dump(t)
-		return "{ " .. table.concat(t, ", ") .. " }"
-	end
-
-	local function teq(t1, t2)
-		if #t1 ~= #t2 then return false end
-		for k, v in pairs(t1) do
+		return "{ " .. fn.reduce(t, function(r, v)
+			local str
 			if type(v) == "table" then
-				if not teq(v, t2[k]) then return false end
-			elseif t2[k] ~= v then return false end
-		end
-		return true
+				str = dump(v)
+			else
+				str = tostring(v)
+			end
+			return r ~= "" and (r .. ", " .. str) or str
+		end, "") .. " }"
 	end
 
 	local function test(t1, t2)
-		if not teq(t1, t2) then
+		if not fn.equal(t1, t2) then
 			print("test failed:")
 			print("\texpected: " .. dump(t1))
 			print("\tgot: " .. dump(t2))
@@ -25,24 +23,21 @@ lib = require("functional");
 		return true
 	end
 
-	local fn = lib
-
 	local lst = { 1, 3, 2, 5, 4 }
 	local tbl = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5 }
 
-	-- self sanity check
-	assert(false == teq({1, 2, 3}, {1, 5, 7}))
-	assert(true == teq({1, 2, 3}, {1, 2, 3}))
-
 	-- all
+	assert(true == fn.all({}, function(v) return v > 0 end))
 	assert(true == fn.all(lst, function(v) return v > 0 end))
 	assert(false == fn.all(lst, function(v) return v > 1 end))
 
 	-- any
+	assert(false == fn.any({}, function(v) return v > 0 end))
 	assert(true == fn.any(lst, function(v) return v > 3 end))
 	assert(false == fn.any(lst, function(v) return v > 10 end))
 
 	-- binary_search
+	assert(nil == fn.binary_search({}, 1))
 	assert(1 == fn.binary_search(fn.sorted(lst), 1))
 	assert(2 == fn.binary_search(fn.sorted(lst), 2))
 	assert(3 == fn.binary_search(fn.sorted(lst), 3))
@@ -51,6 +46,7 @@ lib = require("functional");
 	assert(nil == fn.binary_search(fn.sorted(lst), 6))
 
 	-- clone
+	assert(test({}, fn.clone({})))
 	assert(test(lst, fn.clone(lst)))
 
 	-- concat
@@ -61,34 +57,78 @@ lib = require("functional");
 	assert(test({ 1, 2, 3, 4, 5 }, fn.concat({ 1, 2 }, { 3, 4 } , { 5 })))
 
 	-- contains
+	assert(false == fn.contains({}, 5))
 	assert(true == fn.contains(lst, 5))
 	assert(false == fn.contains(lst, 6))
 
 	-- each
-	local n = 0
-	fn.each(lst, function(v) n = n + v end)
-	assert(n == fn.sum(lst))
+	do
+		local n = 0
+		fn.each(lst, function(v) n = n + v end)
+		assert(n == fn.sum(lst))
+	end
+
+	-- equal
+	assert(false == fn.equal({1, 2, 3}, {1, 5, 7}))
+	assert(true == fn.equal({1, 2, 3}, {1, 2, 3}))
+	assert(true == fn.equal({1, 2, { 3 } }, {1, 2, { 3 } }))
+	assert(false == fn.equal({1, 2, { 3 } }, {1, 2, { 3 } }, true))
 
 	-- filter
+	assert(test({}, fn.filter({}, function(v) return v > 1 end)))
 	assert(test({ 3, 2, 5, 4 }, fn.filter(lst, function(v) return v > 1 end)))
 
-	-- find
-	assert(5 == fn.find(lst, function(v) return v == 5 end))
-	assert(nil == fn.find(lst, function(v) return v == 6 end))
+	-- find_first_of
+	assert(test({}, { fn.find_first_of({}) }))
+	assert(test({ 3, 2 }, { fn.find_first_of(lst, 3) }))
+	assert(test({ 3, 2 }, { fn.find_first_of(lst, 6, 3) }))
+	assert(test({ 1, 1 }, { fn.find_first_of(lst, 5, 1) }))
+	assert(test({ 4, 5 }, { fn.find_first_of(lst, 6, 7, 4) }))
+
+	-- find_last_of
+	assert(test({}, { fn.find_last_of({}) }))
+	assert(test({ 3, 2 }, { fn.find_last_of(lst, 3) }))
+	assert(test({ 3, 2 }, { fn.find_last_of(lst, 6, 3) }))
+	assert(test({ 5, 4 }, { fn.find_last_of(lst, 5, 1) }))
+	assert(test({ 4, 5 }, { fn.find_last_of(lst, 5, 1, 4) }))
+
+	-- find_if
+	assert(test({}, { fn.find_if({}, function(v) return v == 5 end) }))
+	assert(test({ 5, 4 }, { fn.find_if(lst, function(v) return v == 5 end) }))
+	assert(test({}, { fn.find_if(lst, function(v) return v == 6 end) }))
+
+	-- flatten
+	assert(test({}, fn.flatten({})))
+	assert(test({ 1, 2 }, fn.flatten({ 1, 2 })))
+	assert(test({ 1, 2, 3 }, fn.flatten({ 1, 2, { 3 } })))
+	assert(test({ 1, 2, { 3 } }, fn.flatten({ 1, 2, { { 3 } } }, true)))
+	assert(test({ 1, 2, 3, { 4 } }, fn.flatten({ 1, 2, { 3 }, { { 4 } } }, true)))
+	assert(test({ 1, 2, 3, 4, 5 }, fn.flatten({ 1, 2, { { 3 } }, { 4, 5 } })))
+	assert(test({ 1, 2, 3, 4, 5 }, fn.flatten({ 1, 2, { { 3 } }, { { { 4 } }, 5 } })))
+
+	-- invert
+	assert(test({}, fn.invert({})))
+	assert(test({ ["v"] = "k" }, fn.invert({ ["k"] = "v" })))
+	assert(test({ "a", "b", "c", "d", "e" }, fn.invert(tbl)))
 
 	-- keys
+	assert(test({}, fn.keys({})))
 	assert(test({ "a", "b", "c", "d", "e" }, fn.sorted(fn.keys(tbl))))
 
 	-- map
+	assert(test({}, fn.map({})))
 	assert(test({ 2, 6, 4, 10, 8 }, fn.map(lst, function(v) return v * 2 end)))
 
 	-- max
+	assert(nil == fn.max({}))
 	assert(5 == fn.max(lst))
 
 	-- min
+	assert(nil == fn.min({}))
 	assert(1 == fn.min(lst))
 
 	-- pairs
+	assert(test({}, fn.pairs({})))
 	assert(test({ { "a", 1 }, { "b", 2 } }, fn.pairs({ ["a"] = 1, ["b"] = 2 })))
 
 	-- reduce
@@ -97,24 +137,51 @@ lib = require("functional");
 	assert(15 == fn.reduce(lst, function(r, v) return r + v end))
 	assert(16 == fn.reduce(lst, function(r, v) return r + v end, 1))
 	assert(15 == fn.reduce({ 5 }, function(r, v) return r + v end, 10))
+	assert(1 == fn.reduce({ 8, 4, 2, 1 }, function(r, v) return r / v end))
+
+	-- reduce_right
+	assert(nil == fn.reduce_right({}, function(r, v) return r + v end))
+	assert(5 == fn.reduce_right({ 5 }, function(r, v) return r + v end))
+	assert(1 == fn.reduce_right({ 1, 2, 4, 8 }, function(r, v) return r / v end))
 
 	-- reverse
+	assert(test({}, fn.reverse({})))
 	assert(test({ 4, 5, 2, 3, 1 }, fn.reverse(lst)))
 
+	-- shuffle
+	assert(3 == #fn.shuffle({ 1, 2, 3 }))
+
+	-- shuffled
+	assert(3 == #fn.shuffled({ 1, 2, 3 }))
+
+	-- slice
+	assert(test({ }, fn.slice({}, 0)))
+	assert(test({ 1, 3, 2, 5, 4 }, fn.slice(lst)))
+	assert(test({ 1, 3, 2, 5, 4 }, fn.slice(lst, 1)))
+	assert(test({ 3, 2, 5 }, fn.slice(lst, 2, 4)))
+	assert(test({ 5, 4 }, fn.slice(lst, -2)))
+	assert(test({ 3, 2, 5 }, fn.slice(lst, 2, -1)))
+	assert(test({ 1, 3, 2 }, fn.slice(lst, 1, -2)))
+
 	-- sort
+	assert(test({}, fn.sort({})))
 	assert(test({ 1, 2, 3, 4, 5 }, fn.sort(fn.clone(lst))))
 
 	-- sorted
+	assert(test({}, fn.sorted({})))
 	assert(test({ 1, 2, 3, 4, 5 }, fn.sorted(lst)))
 	assert(test({ 1, 3, 2, 5, 4 }, lst))
 
 	-- sorted_index
+	assert(1 == fn.sorted_index({}, 4))
 	assert(4 == fn.sorted_index({ 1, 2, 3, 4, 5, 6 }, 4))
 
 	-- sorted_insert
+	assert(test({ 4 }, fn.sorted_insert({}, 4)))
 	assert(test({ 1, 2, 3, 4, 5}, fn.sorted_insert({ 1, 2, 3, 5 }, 4)))
 
 	-- sum
+	assert(nil == fn.sum({}))
 	assert(15 == fn.sum(lst))
 
 	-- union
@@ -125,12 +192,29 @@ lib = require("functional");
 	assert(test({ 1, 2, 3, 4, 5 }, fn.union({ 1, 2 }, { 1, 2, 3, 4 } , { 3, 5 })))
 
 	-- uniq
+	assert(test({}, fn.uniq({})))
 	assert(test({ 2, 3, 1 }, fn.uniq({ 2, 3, 2, 1, 1 })))
 	assert(test({ 1, 2, 3 }, fn.uniq({ 1, 1, 2, 2, 3 }, true)))
 	assert(test({ 1, 2 }, fn.uniq({ 1, 1, 2, 2, 3 }, true, function(v) return math.floor(v/2) end)))
 	
+	-- unzip
+	assert(test({}, { fn.unzip({}) }))
+	assert(test({ { 1, 2 } }, { fn.unzip({ { 1 }, { 2 } }) }))
+	assert(test({ { 1, 2 }, { 3, 4 } }, { fn.unzip({ { 1, 3 }, { 2, 4 } }) }))
+	assert(test({ { 1, 2 }, { 3, 4 }, { 5, 6 } }, { fn.unzip({ { 1, 3, 5 }, { 2, 4, 6 } }) }))
+	assert(test({ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, { fn.unzip({ { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } }) }))
+
 	-- values
 	assert(test({ 1, 2, 3, 4, 5 }, fn.sorted(fn.values(tbl))))
+
+	-- zip
+	assert(test({}, fn.zip({})))
+	assert(test({ { 1 }, { 2 } }, fn.zip({ 1, 2 })))
+	assert(test({ { 1, 3 }, { 2, 4 } }, fn.zip({ 1, 2 }, { 3, 4 })))
+	assert(test({ { 1, 3, 5 }, { 2, 4, 6 } }, fn.zip({ 1, 2 }, { 3, 4 }, { 5, 6 })))
+	assert(test({ { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } }, fn.zip({ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 })))
+	assert(test({ { 1, 3, 5 }, { 2, 4, 6 } }, fn.zip({ 1, 2, 10 }, { 3, 4 }, { 5, 6 })))
+	assert(test({ { 1, 3, 5 }, { 2, 4, 6 } }, fn.zip({ 1, 2, 10 }, { 3, 4 }, { 5, 6, 20, 30 })))
 
 	print("LibFunctional-1.0: tests passed")
 end)()
